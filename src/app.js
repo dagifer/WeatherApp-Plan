3a545f3610b9e5390910832d5d24bb23bf3cdceb
dagifer <125514178+dagifer@users.noreply.github.com>
2023-04-29 16:39:06 +0100

    Added id

--- src/app.js
+++ src/app.js
@@ -19,4 +19,5 @@ function showWetaher(response) {
 
 let apiKey = "o36b6dafeb6ef56f34fa0t0eceebce4e";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Edinburgh&key=${apiKey}&units=metric`;
-console.log(apiUrl);
+
+axios.get(apiUrl).then(showWetaher);

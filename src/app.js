730a3598e31eee8494c35aa9355dc45c48d0b666
dagifer <125514178+dagifer@users.noreply.github.com>
2023-04-29 17:33:22 +0100

    Search city API

--- src/app.js
+++ src/app.js
@@ -15,9 +15,22 @@ function showWetaher(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+
+  document.querySelector("#city").innerHTML = response.data.city;
 }
 
-let apiKey = "o36b6dafeb6ef56f34fa0t0eceebce4e";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Edinburgh&key=${apiKey}&units=metric`;
+function searchCity(city) {
+  let apiKey = "o36b6dafeb6ef56f34fa0t0eceebce4e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(showWetaher);
+  axios.get(`${apiUrl}`).then(showWetaher);
+}
+
+function showLocation(position) {
+  let apiKey = "o36b6dafeb6ef56f34fa0t0eceebce4e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${position.coords.latitude}&lon=${position.coords.latitude}&key=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}`).then(showWetaher);
+}
+
+searchCity("Edinburgh");

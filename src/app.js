4b94ba903c88b866b4ea95c3edbb5bc6a532ef7e
dagifer <125514178+dagifer@users.noreply.github.com>
2023-04-30 17:13:40 +0100

    working on the link °C and °F

--- src/app.js
+++ src/app.js
@@ -44,9 +44,9 @@ currentDate.innerHTML = `${date}, ${month} ${year} ${hours}:${minutes}`;
 
 function showWetaher(response) {
   console.log(response);
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.temperature.current
-  );
+
+  celsiusTemp = response.data.temperature.current;
+  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
 
   document.querySelector("#city").innerHTML = response.data.city;
 
@@ -76,6 +76,23 @@ function showWetaher(response) {
     .setAttribute("alt", response.data.condition.description);
 }
 
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let fahrenheit = (celsiusTemp * 9) / 5 + 32;
+  let mainTemperature = document.querySelector("#temperature");
+  celsiusTemperature.classList.remove("active");
+  fahrenheitTemp.classList.add("active");
+  mainTemperature.innerHTML = Math.round(fahrenheit);
+}
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+  let mainTemperature = document.querySelector("#temperature");
+  celsiusTemperature.classList.add("active");
+  fahrenheitTemp.classList.remove("active");
+  mainTemperature.innerHTML = Math.round(celsiusTemp);
+}
+
 function handleSumit(event) {
   event.preventDefault();
   let cityElement = document.querySelector("#city-input");
@@ -95,6 +112,14 @@ function showLocation(position) {
 
   axios.get(`${apiUrl}`).then(showWetaher);
 }
+
+let celsiusTemp = null;
+
+let celsiusTemperature = document.querySelector("#celsius-temp");
+celsiusTemperature.addEventListener("click", showCelsiusTemp);
+
+let fahrenheitTemp = document.querySelector("#fahrenheit-temp");
+fahrenheitTemp.addEventListener("click", showFahrenheitTemp);
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSumit);
